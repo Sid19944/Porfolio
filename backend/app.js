48ec25d2a3b17ad6acd54a94d1c8ffc9c6237e15
Siddharth Sarkar <sidsarkar2112@gmail.com>
2026-01-16 16:01:04 +0530

    fixing cors origin

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.DASHBOARD_URL, process.env.FRONTEND_URL],
+    origin: ["http://localhost:5173",process.env.DASHBOARD_URL, process.env.FRONTEND_URL],
     credentials: true,
   })
 );
